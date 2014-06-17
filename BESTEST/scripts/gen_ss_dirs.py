95527554c3af9874582895d6f8a7049b52c2c325
nealkruis <neal.kruis@bigladdersoftware.com>
2014-06-17 08:44:23 -0600

    Add more SS sensitivity solutions.

--- BESTEST/scripts/gen_ss_dirs.py
+++ BESTEST/scripts/gen_ss_dirs.py
@@ -9,22 +9,18 @@ with open('../SS-Sens/solns.csv','rU') as f:
         dir_name = 'D'+row[0]+'F'+row[1]+'C'+row[2]+'G'+row[3]+'T'+row[4]
         if not os.path.exists('../SS-Sens/'+dir_name):
             os.makedirs('../SS-Sens/'+dir_name)
-        if os.path.exists('../SS-Sens/'+dir_name+'/run.sh'):
-            os.remove('../SS-Sens/'+dir_name+'/run.sh')
-        if os.path.exists('../SS-Sens/'+dir_name+'/jrun.sh'):
-            os.remove('../SS-Sens/'+dir_name+'/jrun.sh')
-        os.symlink('../../bin/run.sh','../SS-Sens/'+dir_name+'/run.sh')
-        os.symlink('../../bin/jrun.sh','../SS-Sens/'+dir_name+'/jrun.sh')
-        with open('../SS-Sens/'+dir_name+'/soln.txt','w') as soln:
-            soln.write('# Set values below\n\n')
-            soln.write(':deep_ground_depth=>'+row[0]+',\n')
-            soln.write(':far_field_width=>'+row[1]+',\n')
-            soln.write(':min_cell_dim=>0.0'+row[2]+',\n')
-            soln.write(':max_cell_growth=>'+row[3][0]+'.'+row[3][1:]+',\n')
-            exp = float(row[4])/10;
-            tol = 10**(-exp)
-            tol_str = str(tol)            
-            soln.write(':linear_solver_tolerance=>'+tol_str)
+            os.symlink('../../bin/run.sh','../SS-Sens/'+dir_name+'/run.sh')
+            os.symlink('../../bin/jrun.sh','../SS-Sens/'+dir_name+'/jrun.sh')
+            with open('../SS-Sens/'+dir_name+'/soln.txt','w') as soln:
+                soln.write('# Set values below\n\n')
+                soln.write(':deep_ground_depth=>'+row[0]+',\n')
+                soln.write(':far_field_width=>'+row[1]+',\n')
+                soln.write(':min_cell_dim=>0.0'+row[2]+',\n')
+                soln.write(':max_cell_growth=>'+row[3][0]+'.'+row[3][1:]+',\n')
+                exp = float(row[4])/10;
+                tol = 10**(-exp)
+                tol_str = str(tol)
+                soln.write(':linear_solver_tolerance=>'+tol_str)
 
 
 if __name__ == '__main__':
